759b48501f739381e67ac9b763d50f31c721f411
Erik Bleckhorns <erik.bleckhorns@gmail.com>
2022-07-26 17:58:51 +0200

    Initial pattern with Auth and Firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,49 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as express from 'express'
-
-// admin.initializeApp({
-//   credential: admin.credential.applicationDefault(),
-//   // TODO: ADD YOUR DATABASE URL
-//   databaseURL: undefined
-// });
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import {DecodedIdToken} from "firebase-admin/auth";
+import * as express from "express";
 
 admin.initializeApp();
 
+const db = admin.firestore();
+
+// This is just a helper to add the type to the db responses
+// const createCollection = <T = DocumentData>(collectionName: string) => {
+//   return collection(firestore, collectionName) as CollectionReference<T>
+// }
+
 const app = express();
 
-// Express authentication middleware
-const authenticate = async (req: any, res: any, next: any) => {
-	functions.logger.log(`GET /api/profile init`);
+// Extending Express Request
+export interface RequestCustom extends express.Request
+{
+    decodedIdToken: DecodedIdToken ;
+}
 
-  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
-  	functions.logger.log(`ERROR GET /api/profile - !req.headers.authorization`);
-    res.status(403).send('Unauthorized');
+// Express authentication middleware
+const authenticate = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction) => {
+// functions.logger.log("GET authenticate init");
+
+  if (!req.headers.authorization ||
+      !req.headers.authorization.startsWith("Bearer ")) {
+    functions.logger.warn("authenticate - req.headers.authorization");
+    res.status(403).send("Unauthorized");
     return;
   }
-  const idToken = req.headers.authorization.split('Bearer ')[1];
+  const idToken = req.headers.authorization.split("Bearer ")[1];
   try {
-  	functions.logger.log(`GET /api/profile - verifyIdToken`);
     const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-    req.user = decodedIdToken;
+    const reqCust = req as RequestCustom;
+    reqCust.decodedIdToken = decodedIdToken;
     next();
     return;
-  } catch(e) {
-    res.status(403).send('Unauthorized');
+  } catch (error) {
+    functions.logger.warn("authenticate - verifyIdToken");
+    functions.logger.warn((error as Error).message);
+    res.status(403).send("Unauthorized");
     return;
   }
 };
@@ -37,27 +51,92 @@ app.use(authenticate);
 
 // GET /api/profile
 // Get profile for current user
-app.get('/api/profile', async (req, res) => {
+app.get("/profile/", async (req, res) => {
+  let userid: string;
 
-	functions.logger.log(`GET /api/profile for current user`);
+  try {
+    const reqCust = req as RequestCustom;
+    userid = reqCust.decodedIdToken.email as string;
+    functions.logger.log("GET /profile for current user" + userid);
+  } catch (error) {
+    functions.logger.error(
+        "ERROR - GET /profile",
+        (error as Error).message
+    );
+    return res.sendStatus(500);
+  }
 
-	try {
-		// @ts-ignore
-		const uid = req.user.uid;
-		
+  try {
+    const resProfiles: Array<{
+      id: string,
+      email: string,
+      firstname: string,
+      lastname: string
+    }>= [];
+    const doc: FirebaseFirestore.DocumentData =
+      await db.collection("profiles").doc(userid).get();
 
-		
-		return res.status(200).json({ uid });
-	} catch(error) {
-		functions.logger.log(
-		  'ERROR - GET /api/profile',
-		  // @ts-ignore
-		  error.message
-		);
-		return res.sendStatus(500);
-	}
+    if (doc.exists) {
+      // console.log(doc.data());
+      const resId: string = doc.id;
+      const resEmail: string = doc.data().email;
+      const resFirstName: string = doc.data().firstName;
+      const resLastName: string = doc.data().lastName;
+
+      resProfiles.push({
+        "id": resId,
+        "email": resEmail,
+        "firstname": resFirstName,
+        "lastname": resLastName,
+      });
+
+      return res.status(200).json(resProfiles);
+    } else {
+      console.log("Profile not found - " + userid);
+      return res.status(200).json("{}");
+    }
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+});
+
+// GET /profiles
+// Get all profiles
+app.get("/profiles", async (req, res) => {
+  try {
+    const resProfiles: Array<{
+      id: string,
+      email: string,
+      firstname: string,
+      lastname: string
+    }>= [];
+    const profilesSnap = await db.collection("profiles").orderBy("email").get();
+
+    profilesSnap.forEach((doc: FirebaseFirestore.DocumentData) => {
+      const resId: string = doc.id;
+      const resEmail: string = doc.data().email;
+      const resFirstName: string = doc.data().firstName;
+      const resLastName: string = doc.data().lastName;
+
+      resProfiles.push({
+        "id": resId,
+        "email": resEmail,
+        "firstname": resFirstName,
+        "lastname": resLastName,
+      });
+      // resProfiles.push(doc.data())
+    });
+
+    // console.log("resProfiles - " + JSON.stringify(resProfiles));
+
+    return res.status(200).json(resProfiles);
+    // return res.status(200).send(JSON.stringify(resProfiles));
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
 });
 
 // Expose API as a function
 exports.api = functions.https.onRequest(app);
-
